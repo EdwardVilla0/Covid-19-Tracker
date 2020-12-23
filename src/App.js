c1b8f078965b59f061da60bef45cc8432b91f580
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2020-12-23 15:12:41 -0800

    adding styling and moving select box to the right

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Menu, Select, Card, FormControl, MenuItem } from '@material-ui/core';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      <h1>lets build a covid tracker</h1>
+      <div className="app_header">
+        <h1>Covid Tracker</h1>
+        <FormControl className="app_dropdown">
+          <Select
+            variant="outlined"
+            value="abs">
+            <MenuItem value="worldwide">Worlwide</MenuItem>
+            <MenuItem value="worldwide">Worlwide</MenuItem>
+            <MenuItem value="worldwide">Worlwide</MenuItem>
+            <MenuItem value="worldwide">Worlwide</MenuItem>
+          </Select>
+        </FormControl>
+
+      </div>
     </div>
   );
 }
